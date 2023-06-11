fd10717e3930b635f23171152d3893b79e089ef2
alejanicki <alessandrojanicki@hotmail.com>
2023-06-11 20:16:09 -0300

    feat: implementing read data function

--- app/app.py
+++ app/app.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+import openpyxl
+
+# Ler dados do arquivo .xlsx
+def read_data_from_excel(file_path):
+    workbook = openpyxl.load_workbook(file_path)
+    sheet = workbook.active
+
+    data = []
+    for row in sheet.iter_rows(values_only=True):
+        data.append(row)
+
+    return data
+
+# Ler dados do arquivo .xlsx
+file_path = 'C:\\repos\\atividade-n2\\data.xlsx'
+data = read_data_from_excel(file_path)
+print(data)
